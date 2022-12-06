5e2cf4d4ba0c320e9861a7e2363813308657eb18
JunJie-GitHub <2388791993@qq.com>
2022-12-06 10:31:23 +0800

    beta change

--- src/main/java/com/honma/droolsspringboot/controller/RulesController.java
+++ src/main/java/com/honma/droolsspringboot/controller/RulesController.java
@@ -4,8 +4,6 @@ import com.honma.droolsspringboot.entity.Order;
 import com.honma.droolsspringboot.service.RulesService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @Author: lijunjie
---
+++ src/main/java/com/honma/droolsspringboot/service/BetaTest.java
@@ -0,0 +1,8 @@
+package com.honma.droolsspringboot.service;
+
+/**
+ * @Author: lijunjie
+ * @Date: 2022/12/6
+ */
+public class BetaTest {
+}
